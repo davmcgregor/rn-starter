683763f07d812da9e33755b404188837323f3e3c
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-02 18:01:56 +0000

    Add button to Homescreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Home</Text>
+      <Button
+        onPress={() => console.log('Button pressed')}
+        title='Go to the Components Demo'
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 76,
+  },
+});
+
+export default HomeScreen;
