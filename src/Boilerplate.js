0958b599f01d81f3817e4f667d5c3db7471b8831
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-03 13:08:00 +0000

    Add image props

--- src/Boilerplate.js
+++ src/Boilerplate.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ListScreen = () => {
+const Boilerplate = () => {
   return <Text>Hi</Text>;
 };
 
 const styles = StyleSheet.create({});
 
-export default ListScreen;
+export default Boilerplate;
