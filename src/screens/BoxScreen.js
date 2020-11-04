771305f829c479985f863e4b0db3656ae0468c0c
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-04 20:52:15 +0000

    Create BoxScreen screen

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.text1Style}>Child #1</Text>
+      <Text style={styles.text2Style}>Child #2</Text>
+      <Text style={styles.text3Style}>Child #3</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200,
+  },
+  text1Style: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  text2Style: {
+    borderWidth: 3,
+    borderColor: 'green',
+    fontSize: 18,
+    ...StyleSheet.absoluteFillObject,
+  },
+  text3Style: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+});
+
+export default BoxScreen;
