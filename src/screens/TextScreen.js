526cc92f99fec5380ef9335c892e2dab62eaa45c
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-04 12:40:23 +0000

    Add validaion to TextScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View>
-      <Text>Enter name: </Text>
+      <Text>Enter Password: </Text>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
-        value={name}
-        onChangeText={(newValue) => setName(newValue)}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
+      {password.length < 4 ? <Text>Password must be 4 characters </Text> : null}
     </View>
   );
 };
